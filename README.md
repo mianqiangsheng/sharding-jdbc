864ba9c135f84f397920d1576bb5728ddb15ee97
mianqiangsheng <lizhen19911120@gmail.com>
2022-01-29 15:34:55 +0800

    学习修改

--- README.md
+++ README.md
@@ -23,3 +23,18 @@ sjdbc-db-sharding-table-read-write-range-group-springboot：Spring Boot版 Shard
 [Sharding-JDBC:垂直拆分怎么做？](https://mp.weixin.qq.com/s/wl8h6LIQUHztVuVbjfsU3Q)
 
 [Sharding-JDBC:单库分表的实现](http://cxytiandi.com/blog/detail/36400)
+
+[一致性hash](https://mp.weixin.qq.com/s/yflzIQFiNa3tDJm7U9P8ig)
+
+# 扩容数据迁移问题
+1、停机发布：好处是简单，风险小；缺点是业务有损。
+2、平滑迁移：从某一点开始设置checkpoint , 然后执行数据双写，最后修改路由，删除旧数据，完成扩容。
+
+# 事务问题
+1、分布式事务：比如seata分布式框架
+2、程序+业务逻辑：业务逻辑按照一定规则将涉及的表操作都落到同一个库上，避免了数据库锁竞争和分布式事务；对于金融属性的强一致要求，采用了非常重的侵入式TCC来保证全局支付事务的一致。
+
+# 查询问题
+之前一个库就能搞定的join，count等各种联合查询，将不复存在，老老实实调接口在代码层面实现吧。
+
+[ZDAL中间件]：可以在分布式环境下看上去像传统数据库一样提供海量数据服务，是一种通用的分库分表数据库访问框架。
--- sjdbc-db-sharding-table-read-write-range-group-springboot/src/main/java/com/cxytiandi/sharding/algorithm/MyPreciseShardingAlgorithm.java
+++ sjdbc-db-sharding-table-read-write-range-group-springboot/src/main/java/com/cxytiandi/sharding/algorithm/MyPreciseShardingAlgorithm.java
@@ -13,6 +13,8 @@ import com.cxytiandi.sharding.config.ShardingRangeConfig;
 
 /**
  * 自定义分片算法
+ *
+ * 考虑到后期扩容数据迁移，最好是使用一致性hash算法来定位数据的分库分表位置，可以最大限度降低迁移涉及的库和表
  * 
  * @author yinjihuan
  *
@@ -34,7 +36,11 @@ public class MyPreciseShardingAlgorithm implements PreciseShardingAlgorithm<Long
 		config.setDatasourceList(Arrays.asList("dss0", "dss1"));
 		configs.add(config);
 	}
-	
+
+	/**
+	 * 针对id从1-30的记录全部只存入ds0、ds1，31-60的记录存入到dss0、dss1....（这里实际情况可能id范围 1-100万）
+	 * 预先估计好未来几年的数据量，一次性建立好库，避免后面扩容迁移数据
+	 */
 	@Override
 	public String doSharding(Collection<String> availableTargetNames, PreciseShardingValue<Long> shardingValue) {
 		Optional<ShardingRangeConfig> configOptional = configs.stream().filter(
